192cc94dee3fff4ada621b9fcab2feed82896f8b
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-04-23 19:01:45 -0300

    Classe codigo de titulo

--- tp1-project/Sources/dominios.cpp
+++ tp1-project/Sources/dominios.cpp
@@ -1,3 +1,26 @@
+void codigoDeTitulo::setCodigo(string codigoDeTitulo){
+    validar(codigoDeTitulo);
+    this->codigoDeTitulo=codigoDeTitulo;
+};
+
+void codigoDeTitulo::validar(string codigoDeTitulo){
+    identificador.assign(codigoDeTitulo,0,3);
+    if (codigoDeTitulo.length()!=11)
+        throw invalid_argument("Codigo de Titulo Invalido.");
+    if (identificador!="CDB" && identificador!="CRA" && identificador!="CRI" && identificador!="LCA" && identificador!="LCI" && identificador!="DEB" )
+        throw invalid_argument("Codigo de Titulo Invalido.");
+    else
+        for (int i=3;i<11;i++){
+            if (!(isdigit(codigoDeTitulo[i])) && !(isupper(codigoDeTitulo[i])))
+                throw invalid_argument("Codigo de Titulo Invalido.");
+        };
+};
+
+string codigoDeTitulo::getCodigo(){
+    return codigoDeTitulo;
+};
+
+
 void Percentual::setValor(int valor){
     validar(valor);
     this->valor=valor;
