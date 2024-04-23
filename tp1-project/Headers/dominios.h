eeaf8dd6bf14f0949d52e43e7116726d4bc875dd
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-04-23 16:04:04 -0300

    Update dominios.h
    Criação e organização mínima das classes

--- tp1-project/Headers/dominios.h
+++ tp1-project/Headers/dominios.h
@@ -1,1 +1,69 @@
+class codigoDePagamento{ 
+private:
 
+public:
+
+};
+
+class codigoDeTitulo{
+private:
+
+public:
+
+};
+
+class CPF{
+private:
+
+public:
+
+};
+
+class Data{
+private:
+
+public:
+
+};
+
+class Dinheiro{
+private:
+
+public:
+
+};
+
+class Estado{
+private:
+
+public:
+
+};
+
+class Nome{
+private:
+
+public:
+
+};
+
+class Percentual{
+private:
+
+public:
+
+};
+
+class Senha{
+private:
+
+public:
+
+};
+
+class Setor{
+private:
+
+public:
+
+};
