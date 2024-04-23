a87f75ea54d1d207a5c28bf233c715e79c61fc09
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-04-23 16:18:15 -0300

    Métodos classe Percentual dominios.cpp
    Métodos classe Percentual

--- tp1-project/Sources/dominios.cpp
+++ tp1-project/Sources/dominios.cpp
@@ -1,1 +1,11 @@
-
+void Percentual::setValor(int valor){
+    validar(valor);
+    this->valor=valor;
+};
+void Percentual::validar(int valor){
+    if (valor < 0 || valor > 100)
+        throw invalid_argument("Percentual Invalido.");
+};
+int Percentual::getValor(){
+    return valor;
+};
