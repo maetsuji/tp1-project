d91b71967e55cf675ba3905f76f07df18574acbc
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-17 11:43:06 -0300

    Update testes.h
    Declaração da classe TUPercentual

--- tp1-project/Headers/testes.h
+++ tp1-project/Headers/testes.h
@@ -1,1 +1,18 @@
 using namespace std;
+
+class TUPercentual {
+private:
+    const static int VALOR_VALIDO   = 97;   // Definição de constante para evitar número mágico.
+    const static int VALOR_INVALIDO = 107;   // Definição de constante para evitar número mágico.
+    Percentual *percentual;                         // Referência para unidade em teste.
+    int estado;                             // Estado do teste.
+    void setUp();                           // Método para criar unidade em teste.
+    void tearDown();                        // Método para destruir unidade em teste.
+    void testarCenarioSucesso();            // Cenário de teste.
+    void testarCenarioFalha();              // Cenário de teste.
+
+public:
+    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
+    int run();                              // Método para executar teste.
+};
