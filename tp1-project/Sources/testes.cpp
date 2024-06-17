350e7cbdbc54bbe705fa0179f27fab3b45b6abe3
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-17 11:54:59 -0300

    Update testes.cpp
    +Métodos da classe TUCodigoDeTitulo

--- tp1-project/Sources/testes.cpp
+++ tp1-project/Sources/testes.cpp
@@ -1,5 +1,36 @@
 using namespace std;
 
+void TUCodigoDeTitulo::setUp(){
+    codigo = new codigoDeTitulo();
+    estado = SUCESSO;
+}
+
+void TUCodigoDeTitulo::tearDown(){
+    delete codigo;
+}
+
+void TUCodigoDeTitulo::testarCenarioSucesso(){
+    try{
+        codigo->setCodigo(VALOR_VALIDO);
+        if (codigo->getCodigo() != VALOR_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        estado = FALHA;
+    }
+}
+
+void TUCodigoDeTitulo::testarCenarioFalha(){
+    try{
+        codigo->setCodigo(VALOR_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        if (codigo->getCodigo() == VALOR_INVALIDO)
+            estado = FALHA;
+    }
+}
+
 void TUPercentual::setUp(){
     percentual = new Percentual();
     estado = SUCESSO;
