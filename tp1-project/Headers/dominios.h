99893db865c99ce8b1aa094f303beabca4029fe7
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-17 09:40:00 -0300

    Update dominios.h
    +using namespace std
    -std::

--- tp1-project/Headers/dominios.h
+++ tp1-project/Headers/dominios.h
@@ -1,3 +1,5 @@
+using namespace std;
+
 class codigoDePagamento{
 private:
 
@@ -17,8 +19,8 @@ public:
 
 class CPF{
 private:
-    string CPF
-    int validchar
+    string CPF;
+    int validchar;
 public:
     void setCPF(string);
     void validarCPF(string);
@@ -49,24 +51,24 @@ public:
 
 class Estado{
 private:
-    std::string estado;
-    void validarEstado(const std::string& estado) const;
+    string estado;
+    void validarEstado(const string& estado) const;
 public:
-    Estado(const std::string& novoEstado);
-    std::string getEstado() const;
-    void setEstado(const std::string& novoEstado);
+    Estado(const string& novoEstado);
+    string getEstado() const;
+    void setEstado(const string& novoEstado);
 };
 
 class Nome{
 private:
-    std::string primeiroTermo;
-    std::string segundoTermo;
-    bool validarTermo(const std::string& termo) const;
+    string primeiroTermo;
+    string segundoTermo;
+    bool validarTermo(const string& termo) const;
 
 public:
-    Nome(const std::string& nomeCompleto);
-    std::string getPrimeiroTermo() const;
-    std::string getSegundoTermo() const;
+    Nome(const string& nomeCompleto);
+    string getPrimeiroTermo() const;
+    string getSegundoTermo() const;
 };
 
 class Percentual{
@@ -89,10 +91,10 @@ public:
 
 class Setor{
 private:
-    std::string nome;
-    void validarNome(const std::string& nome) const;
+    string nome;
+    void validarNome(const string& nome) const;
 public:
-    Setor(const std::string& novoNome);
-    std::string getNome() const;
-    void setNome(const std::string& novoNome);
+    Setor(const string& novoNome);
+    string getNome() const;
+    void setNome(const string& novoNome);
 };
