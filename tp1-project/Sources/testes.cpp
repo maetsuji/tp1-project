59929d684c7eb4a8a2c732fcd3d9d859e8da2f2c
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-17 11:43:54 -0300

    Update testes.cpp
    Métodos TUPercentual

--- tp1-project/Sources/testes.cpp
+++ tp1-project/Sources/testes.cpp
@@ -1,1 +1,40 @@
 using namespace std;
+
+void TUPercentual::setUp(){
+    percentual = new Percentual();
+    estado = SUCESSO;
+}
+
+void TUPercentual::tearDown(){
+    delete percentual;
+}
+
+void TUPercentual::testarCenarioSucesso(){
+    try{
+        percentual->setValor(VALOR_VALIDO);
+        if (percentual->getValor() != VALOR_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        estado = FALHA;
+    }
+}
+
+void TUPercentual::testarCenarioFalha(){
+    try{
+        percentual->setValor(VALOR_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao){
+        if (percentual->getValor() == VALOR_INVALIDO)
+            estado = FALHA;
+    }
+}
+
+int TUPercentual::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
