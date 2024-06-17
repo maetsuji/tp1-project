063621d57697f996398f28bd5f522b617b9a2e0f
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-17 11:51:57 -0300

    Update testes.h
    Declaração TUCodigoDeTitulo

--- tp1-project/Headers/testes.h
+++ tp1-project/Headers/testes.h
@@ -1,5 +1,22 @@
 using namespace std;
 
+class TUCodigoDeTitulo {
+private:
+    const string VALOR_VALIDO   = "CRA1913576A";   // Definição de constante para evitar número mágico.
+    const string VALOR_INVALIDO = "GWM12345678";   // Definição de constante para evitar número mágico.
+    codigoDeTitulo *CodigoDeTitulo;                         // Referência para unidade em teste.
+    int estado;                             // Estado do teste.
+    void setUp();                           // Método para criar unidade em teste.
+    void tearDown();                        // Método para destruir unidade em teste.
+    void testarCenarioSucesso();            // Cenário de teste.
+    void testarCenarioFalha();              // Cenário de teste.
+
+public:
+    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
+    int run();                              // Método para executar teste.
+};
+
 class TUPercentual {
 private:
     const static int VALOR_VALIDO   = 97;   // Definição de constante para evitar número mágico.
@@ -16,3 +33,5 @@ public:
     const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
     int run();                              // Método para executar teste.
 };
+
+
