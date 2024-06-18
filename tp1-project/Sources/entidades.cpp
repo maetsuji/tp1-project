63c24cd79a77d1cc599fdd75e923281f31b7221f
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-18 18:56:16 -0300

    Update entidades.cpp
    Class Titulo

--- tp1-project/Sources/entidades.cpp
+++ tp1-project/Sources/entidades.cpp
@@ -24,5 +24,42 @@ Senha Conta::getSenha() const{
 }
 //Class Titulo
 
+void Titulo::setCodigo(const codigoDeTitulo& codigo){
+    this->codigo = codigo;
+}
+void Titulo::setEmissor(const Nome& emissor){
+    this->emissor = emissor;
+}
+void Titulo::setSetor(const Setor& setor){
+    this->setor = setor;
+}
+void Titulo::setEmissao(const Data& emissao){
+    this->emissao = emissao;
+}
+void Titulo::void setVencimento(const Data& vencimento){
+    this->vencimento = vencimento;
+}
+void Titulo::setDinheiro(const Valor& valor){
+    this->valor = valor;
+}
+codigoDeTitulo Titulo::getCodigo() const{
+    return codigo;
+}
+Nome Titulo::getEmissor() const{
+    return emissor;
+}
+Setor Titulo::getSetor() const{
+    return setor;
+}
+Data Titulo::getEmissao() const{
+    return emissao;
+}
+Data Titulo::getVencimento() const{
+    return vencimento;
+}
+Dinheiro Titulo::getValor() const{
+    return valor;
+}
+
 
 //Class Pagamento
