799b42c82125c0bf83d9478cc90150738bc03755
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-18 19:07:53 -0300

    Update entidades.cpp
    Class Pagamento

--- tp1-project/Sources/entidades.cpp
+++ tp1-project/Sources/entidades.cpp
@@ -22,44 +22,86 @@ Nome Conta::getNome() const{
 Senha Conta::getSenha() const{
     return senha;
 }
-//Class Titulo
 
+//Class Titulo
 void Titulo::setCodigo(const codigoDeTitulo& codigo){
     this->codigo = codigo;
 }
+
 void Titulo::setEmissor(const Nome& emissor){
     this->emissor = emissor;
 }
+
 void Titulo::setSetor(const Setor& setor){
     this->setor = setor;
 }
+
 void Titulo::setEmissao(const Data& emissao){
     this->emissao = emissao;
 }
+
 void Titulo::void setVencimento(const Data& vencimento){
     this->vencimento = vencimento;
 }
+
 void Titulo::setDinheiro(const Valor& valor){
     this->valor = valor;
 }
+
 codigoDeTitulo Titulo::getCodigo() const{
     return codigo;
 }
+
 Nome Titulo::getEmissor() const{
     return emissor;
 }
+
 Setor Titulo::getSetor() const{
     return setor;
 }
+
 Data Titulo::getEmissao() const{
     return emissao;
 }
+
 Data Titulo::getVencimento() const{
     return vencimento;
 }
+
 Dinheiro Titulo::getValor() const{
     return valor;
 }
 
-
 //Class Pagamento
+void Pagamento::setCodigo(const codigoDePagamento& codigo){
+    this->codigo = codigo;
+}
+
+void Pagamento::setData(const Data& data){
+    this->data = data;
+}
+
+void Pagamento::setPercentual(const Percentual& percentual){
+    this->percentual = percentual;
+}
+
+void Pagamento::setEstado(const Estado& estado){
+    this->estado = estado;
+}
+
+codigoDePagamento Pagamento::getCodigo() const{
+    return codigo;
+}
+
+Data Pagamento::getData() const{
+    return data;
+}
+
+Percentual Pagamento::getPercentual() const{
+    return percentual;
+}
+
+Estado Pagamento::getEstado() const{
+    return estado;
+}
+
