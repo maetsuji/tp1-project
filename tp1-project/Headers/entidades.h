33ad17460ce35de4b512f6cc6522153882e9fcc1
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-18 18:55:40 -0300

    Update entidades.h
    Class Titulo

--- tp1-project/Headers/entidades.h
+++ tp1-project/Headers/entidades.h
@@ -14,3 +14,26 @@ public:
     Nome getNome() const;
     Senha getSenha() const;
 };
+
+class Titulo{
+private:
+    codigoDeTitulo codigo;
+    Nome emissor;
+    Setor setor;
+    Data emissao;
+    Data vencimento;
+    Dinheiro valor;
+public:
+    void setCodigo(const codigoDeTitulo&);
+    void setEmissor(const Nome&);
+    void setSetor(const Setor&);
+    void setEmissao(const Data&);
+    void setVencimento(const Data&);
+    void setDinheiro(const Valor&);
+    codigoDeTitulo getCodigo() const;
+    Nome getEmissor() const;
+    Setor getSetor() const;
+    Data getEmissao() const;
+    Data getVencimento() const;
+    Dinheiro getValor() const;
+};
