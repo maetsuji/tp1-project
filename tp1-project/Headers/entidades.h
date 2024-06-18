d61909ecc7ae9fd440965d731c30127a1045d104
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-18 18:40:01 -0300

    Update entidades.h
    class Conta

--- tp1-project/Headers/entidades.h
+++ tp1-project/Headers/entidades.h
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+class Conta{
+private:
+    Cpf cpf;
+    Nome nome;
+    Senha senha;
+public:
+    void setCpf(const Cpf&);
+    void setNome(const Nome&);
+    void setSenha(const Senha&);
+    Cpf getCpf() const;
+    Nome getNome() const;
+    Senha getSenha() const;
+};
