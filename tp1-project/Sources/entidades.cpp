d9892fc71ea662843b5f24db5f33ad8156d4f19c
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-18 18:40:55 -0300

    Update entidades.cpp
    class Conta

--- tp1-project/Sources/entidades.cpp
+++ tp1-project/Sources/entidades.cpp
@@ -1,1 +1,28 @@
+//Class Conta
+void Conta::setCpf(const Cpf& cpf){
+    this->cpf = cpf;
+}
 
+void Conta::setNome(const Nome& nome){
+    this->nome = nome;
+}
+
+void Conta::setSenha(const Senha& senha){
+    this->senha = senha;
+}
+
+Cpf Conta::getCpf() const{
+    return cpf;
+}
+
+Nome Conta::getNome() const{
+    return nome;
+}
+
+Senha Conta::getSenha() const{
+    return senha;
+}
+//Class Titulo
+
+
+//Class Pagamento
