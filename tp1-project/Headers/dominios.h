0756de012d9c2109573a55d8cc2c219f871c3425
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-06-19 19:16:20 -0300

    Update dominios.h
    Update Percentual

--- tp1-project/Headers/dominios.h
+++ tp1-project/Headers/dominios.h
@@ -82,8 +82,6 @@ public:
 class Percentual{
 private:
     int valor;
-    static const int valorMinimo = 0;
-    static const int valorMaximo = 100;
 public:
     void setValor(int);
     void validar(int);
