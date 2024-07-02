52a0e37f245235f4689b437401650d0a8f165504
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-07-02 15:27:02 -0300

    Update testes.h - Limpeza de comentários

--- tp1-project/Headers/testes.h
+++ tp1-project/Headers/testes.h
@@ -2,7 +2,7 @@ using namespace std;
 
 //TESTES DE DOMÍNIO
 
-class TUCodigoDeTitulo {
+class TUCodigoDeTitulo {                   //IMPLEMENTADA POR 231028746
 private:
     const string VALOR_VALIDO   = "CRA1913576A";   // Definição de constante para evitar número mágico.
     const string VALOR_INVALIDO = "GWM12345678";   // Definição de constante para evitar número mágico.
@@ -21,9 +21,9 @@ public:
 
 class TUCPF {
 private:
-    const static string VALOR_VALIDO    = "761.776.634-35";  // CPF fictício válido
-    const static string VALOR_INVALIDO  = "099.000.999-88";  // CPF fictício inválido
-    CPF *cpf;                                   // Referência para unidade em teste.
+    const static string VALOR_VALIDO    = "761.776.634-35";   // CPF fictício válido
+    const static string VALOR_INVALIDO  = "099.000.999-88";   // CPF fictício inválido
+    CPF *cpf;                                                 // Referência para unidade em teste.
     int estado;
     void setUp();
     void tearDown();
@@ -38,8 +38,8 @@ public:
 
 class TUDinheiro {
 private:
-    const static float VALOR_VALIDO    = 7343.57;  // Valor válido
-    const static float VALOR_INVALIDO  = 4000999.69;  // Valor inválido
+    const static float VALOR_VALIDO    = 7343.57;      // Valor válido
+    const static float VALOR_INVALIDO  = 4000999.69;   // Valor inválido
     Dinheiro *valor;                                   // Referência para unidade em teste.
     int estado;
     void setUp();
@@ -56,7 +56,7 @@ class TUData {
 private:
     const string VALOR_VALIDO   = "29-02-2020";   // Definição de constante para evitar valores mágicos.
     const string VALOR_INVALIDO = "31-02-2021";   // Definição de constante para evitar valores mágicos.
-    Data *data;                             // Referência para a unidade em teste.
+    Data *data;                                   // Referência para a unidade em teste.
     int estado;
     void setUp();
     void tearDown();
@@ -72,9 +72,9 @@ public:
 
 class TUEstado {
 private:
-    const static string VALOR_VALIDO    = "Previsto";  //Estado fictício válido
+    const static string VALOR_VALIDO    = "Previsto";    //Estado fictício válido
     const static string VALOR_INVALIDO  = "Imprevisto";  //Estado fictício inválido
-    Estado *estado;                         // Referência para unidade em teste.
+    Estado *estado;                                      // Referência para unidade em teste.
     int estado;
     void setUp();
     void tearDown();
@@ -89,9 +89,9 @@ public:
 
 class TUNome {
 private:
-    const static string NOME_VALIDO  = "João Silva";   // Definição de constante para evitar Nomes mágicos.
-    const static string NOME_INVALIDO = "123456"; // Definição de constante para evitar Nomes mágicos.
-    Nome *nome;                             // Referência para a unidade em teste.
+    const static string NOME_VALIDO  = "João Silva";    // Definição de constante para evitar Nomes mágicos.
+    const static string NOME_INVALIDO = "123456";       // Definição de constante para evitar Nomes mágicos.
+    Nome *nome;                                         // Referência para a unidade em teste.
     int estado;
     void setUp();
     void tearDown();
@@ -104,11 +104,11 @@ public:
     int run();
 };
 
-class TUPercentual {
+class TUPercentual {                           //IMPLEMENTADA POR 231028746
 private:
-    const static int VALOR_VALIDO   = 97;    // Definição de constante para evitar número mágico.
-    const static int VALOR_INVALIDO = 107;   // Definição de constante para evitar número mágico.
-    Percentual *percentual;                         // Referência para unidade em teste.
+    const static int VALOR_VALIDO   = 97;      // Definição de constante para evitar número mágico.
+    const static int VALOR_INVALIDO = 107;     // Definição de constante para evitar número mágico.
+    Percentual *percentual;                    // Referência para unidade em teste.
     int estado;
     void setUp();
     void tearDown();
@@ -141,7 +141,7 @@ public:
 
 //TESTES DE ENTIDADE
 
-class TUConta{
+class TUConta{                               //IMPLEMENTADA POR 231028746
 private:
     const int CPF_VALIDO="063.179.831-51";  // Definição de constante para evitar número mágico.
     const string NOME_VALIDO="Caio Rafik";  // Definição de constante para evitar número mágico.
@@ -176,7 +176,7 @@ public:
     int run();                              
 };
 
-class TUPagamento{
+class TUPagamento{                   //IMPLEMENTADA POR 231028746
 private:
     const int CODIGO_VALIDO="789456123";  
     const string DATA_VALIDA = "01/01/2023"; 
