2bb29dceb6a27bc33836fe6671f78ed651290e5d
cmpc29 <165804430+cmpc29@users.noreply.github.com>
2024-07-30 17:18:42 -0300

    Update controladoras.h - CntrAprConta implementada + inlines

--- tp1-project/Headers/controladoras.h
+++ tp1-project/Headers/controladoras.h
@@ -3,19 +3,31 @@ using namespace std;
 class CntrAprConta:public IAprConta{
 private:
     IServConta *cntrservconta;
+    void lerConta(CPF);
+    void atualizarConta(CPF*);
+    void excluirConta(CPF*)
 public:
     void criarConta();
     int executar(CPF*);
     void setCntrServConta(IServConta*);
 };
 
+inline void CntrAprConta::setCntrServConta(IServConta *cntrservconta){
+    this->cntrservconta=cntrservconta
+};
+
 class CntrAprAuth:public IAprAuth{
 private:
     IServAuth *cntrservauth;
 public:
-    inline void setCntrServAuth(IServAuth*);
+    void setCntrServAuth(IServAuth*);
     int executar(CPF*);
 };
+
+inline void CntrAprAuth::setCntrServAuth(IServAuth *cntrservauth){
+    this->cntrservauth=cntrservauth
+};
+
 
 class CntrAprInvestimento:public IAprInvestimento{
 private:
@@ -25,8 +37,13 @@ public:
     int executar(CPF*);
 };
 
-criar{
+inline void CntrAprInvestimento::setCntrServInvestimento(IServInvestimento *cntrservinvestimento){
+    this->cntrservinvestimento=cntrservinvestimento
+};
 
+
+//EXEMPLO PARA AJUDAR NA LÓGICA
+criar{
 try
 cin CPF
 cpf.setValor
