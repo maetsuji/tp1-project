908c0278aca9d95258358d116f1fc87cee609b6c
Cauê Trindade <135842997+cauetrd@users.noreply.github.com>
2024-08-05 21:48:32 -0300

    Update dbService.cpp

--- tp1-project/Sources/dbService.cpp
+++ tp1-project/Sources/dbService.cpp
@@ -149,4 +149,127 @@ bool ComandoExisteConta::getResultado() {
         return true;
     }
 }
-//-----------------------------------------------------------
+//Implementação de métodos relativos a Titulo-----------------------------------------------------------
+
+//CREATE=======================================================
+ComandoCreateTitulo::ComandoCreateTitulo(Titulo titulo){
+    comandoSQL = "INSERT INTO Titulo VALUE (";
+    comandoSQL += "'" + titulo.getCodigo().getValor() + "', ";
+    comandoSQL += "'" + titulo.getConta().getValor() + "', ";
+    comandoSQL += "'" + titulo.getEmissor().getValor() + "', ";
+    comandoSQL += "'" + titulo.getSetor().getValor() + "', ";
+    comandoSQL += "'" + titulo.getEmissao().getValor() + "', ";
+    comandoSQL += "'" + titulo.getVencimento().getValor() + "', ";
+    comandoSQL += "'" + titulo.getDinheiro().getValor() + "')";
+}
+//FIND TITULO============================================================
+ComandoFindTitulo::ComandoFindTitulo(codigoDeTitulo codigo){
+    comandoSQL = "SELECT * FROM Titulo WHERE CodigoTitulo = ";
+    comandoSQL += codigo.getValor();
+}
+
+Conta ComandoFindTitulo::getResultado() {
+    ElementoResultado resultado;
+    Titulo titulo;
+
+    // Remover nome;
+        if (listaResultado.empty())
+                throw EErroPersistencia("Lista de resultados vazia.");
+        resultado = listaResultado.back();
+        listaResultado.pop_back();
+        titulo.setCodigo(Codigo(resultado.getValorColuna()));
+
+        // Remover cpf;
+        if (listaResultado.empty())
+                throw EErroPersistencia("Lista de resultados vazia.");
+        resultado = listaResultado.back();
+        listaResultado.pop_back();
+        conta.setCPF(CPF(resultado.getValorColuna()));
+
+        // Remover senha;
+        if (listaResultado.empty())
+                throw EErroPersistencia("Lista de resultados vazia.");
+        resultado = listaResultado.back();
+        listaResultado.pop_back();
+        conta.setSenha(Senha(resultado.getValorColuna()));
+
+        return conta;
+}
+//UPDATE====================================================
+ComandoUpdateTitulo::ComandoUpdateTitulo(Titulo titulo) {
+    comandoSQL = "UPDATE Titulo ";
+    comandoSQL += "SET Emissor = '" + conta.getNome().getValor();
+    comandoSQL += "', Setor = '" + conta.getSenha().getValor();
+    comandoSQL += "', Emissao = '" + conta.getSenha().getValor
+    comandoSQL += "', Vencimento = '" + conta.getSenha().getValor();
+    comandoSQL += "', Dinheiro = '" + conta.getSenha().getValor();
+    comandoSQL += "' WHERE CodigoTitulo = " + conta.getCPF().getValor();
+}
+
+//DELETE====================================================
+ComandoDeleteTitulo::ComandoDeleteTitulo(codigoDeTitulo codigo) {
+    comandoSQL = "DELETE FROM Titulo WHERE CodigoTitulo = ";
+    comandoSQL += codigo.getValor();
+}
+
+//Implementação de métodos relativos a Pagamento-----------------------------------------------------------
+
+//CREATE=======================================================
+ComandoCreatePagamento::ComandoCreatePagamento(Pagamento pagamento){
+    comandoSQL = "INSERT INTO Titulo VALUE (";
+    comandoSQL += "'" + titulo.getCodigo().getValor() + "', ";
+    comandoSQL += "'" + titulo.getConta().getValor() + "', ";
+    comandoSQL += "'" + titulo.getEmissor().getValor() + "', ";
+    comandoSQL += "'" + titulo.getSetor().getValor() + "', ";
+    comandoSQL += "'" + titulo.getEmissao().getValor() + "', ";
+    comandoSQL += "'" + titulo.getVencimento().getValor() + "', ";
+    comandoSQL += "'" + titulo.getDinheiro().getValor() + "')";
+}
+
+//FIND============================================================
+ComandoFindPagamento::ComandoFindPagamento(codigoDePagamento codigo){
+    comandoSQL = "SELECT * FROM Pagamento WHERE CodigoPagamento = ";
+    comandoSQL += codigo.getValor();
+}
+
+Conta ComandoFindPagamento::getResultado() {
+    ElementoResultado resultado;
+    Pagamento pagamento;
+
+    // Remover nome;
+        if (listaResultado.empty())
+                throw EErroPersistencia("Lista de resultados vazia.");
+        resultado = listaResultado.back();
+        listaResultado.pop_back();
+        titulo.setCodigo(Codigo(resultado.getValorColuna()));
+
+        // Remover cpf;
+        if (listaResultado.empty())
+                throw EErroPersistencia("Lista de resultados vazia.");
+        resultado = listaResultado.back();
+        listaResultado.pop_back();
+        conta.setCPF(CPF(resultado.getValorColuna()));
+
+        // Remover senha;
+        if (listaResultado.empty())
+                throw EErroPersistencia("Lista de resultados vazia.");
+        resultado = listaResultado.back();
+        listaResultado.pop_back();
+        conta.setSenha(Senha(resultado.getValorColuna()));
+
+        return conta;
+}
+//UPDATE====================================================
+ComandoUpdatePagamento::ComandoUpdatePagamento(Pagamento pagamento) {
+    comandoSQL = "UPDATE Pagamento ";
+    comandoSQL += "SET Data = '" + conta.getNome().getValor();
+    comandoSQL += "', Percentual = '" + conta.getSenha().getValor();
+    comandoSQL += "', Estado = '" + conta.getSenha().getValor;
+    comandoSQL += "' WHERE CodigoPagamento = " + conta.getCPF().getValor();
+}
+
+//DELETE====================================================
+ComandoDeletePagamento::ComandoDeletePagamento (codigoDePagamento codigo) {
+    comandoSQL = "DELETE FROM Pagamento WHERE CodigoPagamento = ";
+    comandoSQL += codigo.getValor();
+}
