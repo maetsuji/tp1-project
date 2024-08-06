c7f693f43980cf12e6f173a5be34c162cb5193da
Ricardo de Carvalho Nabuco <131546157+ricardocnabuco@users.noreply.github.com>
2024-08-05 22:49:22 -0300

    Update interfaces.h
    Implementando a Interface de Apresentação de Conta

--- tp1-project/Headers/interfaces.h
+++ tp1-project/Headers/interfaces.h
@@ -8,12 +8,40 @@ class ISInvestimento;
 
 //Camada de Apresentação
 
-//Interface apresentação Conta
-class IAprConta{
+/**
+ * Interface para apresenta&ccedil;&atilde;o de Conta.
+ *
+ * Fun&ccedil;&otilde;es principais:
+ *
+ * - Cria uma nova conta.
+ * - Executa opera&ccedil;&otilde;es com base no CPF.
+ * - Define o controlador de servi&ccedil;os de conta.
+ */
+class IAprConta {
 public:
+    /**
+     * Cria uma nova conta.
+     */
     virtual void criarConta() = 0;
+
+    /**
+     * Executa uma opera&ccedil;&atilde;o com base no CPF fornecido.
+     * 
+     * @param cpf Ponteiro para o CPF.
+     * @return Um inteiro representando o resultado da opera&ccedil;&atilde;o.
+     */
     virtual int executar(CPF*) = 0;
+
+    /**
+     * Define o controlador de servi&ccedil;os de conta.
+     * 
+     * @param servConta Ponteiro para o controlador de servi&ccedil;os de conta.
+     */
     virtual void setCntrServConta(IServConta*) = 0;
+
+    /**
+     * Destrutor virtual para garantir a libera&ccedil;&atilde;o correta dos recursos.
+     */
     virtual ~IAprConta(){};
 };
 
